45bdea49718f2d28c17429e79cc3fa2be8a741b8
Albertlungu <132965708+Albertlungu@users.noreply.github.com>
2025-10-10 17:08:58 -0400

    Created infrastructure for player inventory

---
+++ UI/Inventory/Scripts/item_data.gd
@@ -0,0 +1,7 @@
+extends Resource
+class_name ItemData
+
+@export var name: String = "Gem of Wit"
+@export_multiline var description: String = "DESCRIPTION OF GEM HERE"
+@export var stackable: bool = false
+@export var model: Texture
